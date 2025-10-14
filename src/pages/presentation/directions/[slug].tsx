c3d6476082dae44ec3c1a269ee500bfe453ea5b6
aziz coder 3.0 <azizcoder3.0@gmail.com>
2025-10-14 15:48:12 +0100

    fix: corrigé du fichier slug

--- src/pages/presentation/directions/[slug].tsx
+++ src/pages/presentation/directions/[slug].tsx
@@ -172,7 +172,7 @@ const DirectoratePage: NextPage<DirectoratePageProps> = ({ directorate }) => {
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                   </div>
-                  <h2 className="text-3xl lg:text-4xl font-bold text-gray-800">Missions Clés</h2>
+                  <h2 className="text-3xl lg:text-4xl font-bold text-gray-800">Services</h2>
                 </div>
                 {directorate.services && directorate.services.length > 0 ? (
                   <ul className="space-y-4">
